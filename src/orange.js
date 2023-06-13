f5615a029ef78c01c770b26bff1beab83f3834b5
Ditochkin <ditochkin@bk.ru>
2023-06-13 12:32:58 +0300

    finish

--- src/orange.js
+++ src/orange.js
@@ -3,53 +3,10 @@ let gl = null;
 function initWebGl(canvas) {
     gl = canvas.getContext("webgl");
 
-    if (!gl) {
-        console.log("WebGL not supported")
-        gl = canvas.getContext("experimental-webgl");
-    }
-
-    if (!gl) {
-        alert("Your browser does not support WebGL");
-    }
-
     gl.clearColor(1.0, 1.0, 1.0, 1);
     gl.enable(gl.DEPTH_TEST);
     gl.depthFunc(gl.LEQUAL);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-}
-
-function loadShader(gl, type, source) {
-    let shader = gl.createShader(type);
-    gl.shaderSource(shader, source);
-    gl.compileShader(shader);
-    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-        console.error("Error! Shader compile status ", gl.getShaderInfoLog(shader));
-        return;
-    }
-    return shader;
-}
-
-function initShaderProgram(gl, vsSource, fsSource) {
-    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
-    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
-
-    const shaderProgram = gl.createProgram();
-    gl.attachShader(shaderProgram, vertexShader);
-    gl.attachShader(shaderProgram, fragmentShader);
-
-    gl.linkProgram(shaderProgram);
-    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-        console.error("Error! Link program", gl.getProgramInfoLog(shaderProgram));
-        return;
-    }
-
-    gl.validateProgram(shaderProgram)
-    if (!gl.getProgramParameter(shaderProgram, gl.VALIDATE_STATUS)) {
-        console.error("Error! validate program", gl.getProgramInfoLog(shaderProgram));
-        return;
-    }
-
-    return shaderProgram;
 }
 
 function radian(deg) {
